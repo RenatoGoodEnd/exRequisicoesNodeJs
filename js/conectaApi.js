51ba12a7f37af7dcb86e11c01a4c990cf089cda3
RenatoB_Alves <44118628+RenatoGoodEnd@users.noreply.github.com>
2023-02-07 16:51:39 -0300

    criação da função criaVideo

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -6,6 +6,25 @@ async function listaDeVideos(){
     return listaConvertida;
 }
 
+async function criaVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application.json"
+        },
+        body:json.stringfy({
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    
+    });
+    const conexaoConvertida = await conexao.json();
+    return conexaoConvertida;
+}
+
 export const conectaApi = {
-    listaDeVideos
+    listaDeVideos,
+    criaVideo
 }
